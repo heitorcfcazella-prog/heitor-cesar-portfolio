719874468fb9eac977b63e123c165d6efc5f5b02
Heitor César <heitorcfcazella@gmail.com>
2026-09-13 15:31:31 -0300

    correções de erros no branch merge

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-function copiarEmail(event, email) {
-  event.preventDefault(); 
-  navigator.clipboard.writeText(email);
-}
-
-function fecharMenu() {
-  const menu = document.querySelector('.menu-container');
-  menu.removeAttribute('open');
-}
-
-const themeToggleBtn = document.getElementById('theme-toggle');
-
-// Recupera o tema salvo no navegador ou usa Dark como padrão
-const currentTheme = localStorage.getItem('theme');
-if (currentTheme === 'light') {
-  document.documentElement.setAttribute('data-theme', 'light');
-}
-
-// Altera o tema ao clicar no botão
-themeToggleBtn.addEventListener('click', () => {
-  const isLight = document.documentElement.getAttribute('data-theme') === 'light';
-  
-  if (isLight) {
-    document.documentElement.removeAttribute('data-theme');
-    localStorage.setItem('theme', 'dark');
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('theme', 'light');
-  }
-});
-=======
 function copiarEmail(event, email) {
   event.preventDefault(); 
   navigator.clipboard.writeText(email).then(() => {
@@ -71,5 +39,4 @@ function initThemeToggle() {
   });
 }
 
-initThemeToggle()
->>>>>>> origin/master
+initThemeToggle();
